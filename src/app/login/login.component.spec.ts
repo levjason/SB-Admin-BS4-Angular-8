4989321310384400c98ec4529d217f3f52390a9e
Jason Levine <levjason@gmail.com>
2020-10-29 15:21:54 -0400

    TranslateService provider for LoginComponent test

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { LoginComponent } from './login.component';
 import { LoginModule } from './login.module';
@@ -12,7 +12,16 @@ describe('LoginComponent', () => {
     let fixture: ComponentFixture<LoginComponent>;
       beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [LoginModule, RouterTestingModule, BrowserAnimationsModule, TranslateModule]
+            imports: [LoginModule, RouterTestingModule, BrowserAnimationsModule, 
+                TranslateModule.forRoot({
+                    loader : {
+                        provide: TranslateLoader,
+                        useClass: TranslateFakeLoader
+                    }
+                })],
+            providers: [
+                TranslateService
+            ]
         }).compileComponents();
     }); 
     beforeEach(() => {
