f9fb284c3d32bf5ed7fd03cf13770bf1f2642df6
Jason Levine <levjason@gmail.com>
2020-10-29 16:01:14 -0400

    Tests for access-denied and not found components

--- src/app/access-denied/access-denied.component.spec.ts
+++ src/app/access-denied/access-denied.component.spec.ts
@@ -1,16 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { AccessDeniedComponent } from './access-denied.component';
+import { AccessDeniedModule } from './access-denied.module';
 
 describe('AccessDeniedComponent', () => {
     let component: AccessDeniedComponent;
     let fixture: ComponentFixture<AccessDeniedComponent>;
 
-    beforeEach(async(() => {
+    beforeEach(async() => {
         TestBed.configureTestingModule({
+            imports: [AccessDeniedModule, 
+                TranslateModule.forRoot({
+                    loader : {
+                        provide: TranslateLoader,
+                        useClass: TranslateFakeLoader
+                    }
+                })],
+                providers: [
+                    TranslateService
+                ],
             declarations: [AccessDeniedComponent]
         }).compileComponents();
-    }));
+    });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(AccessDeniedComponent);
---
+++ src/app/not-found/not-found.component.spec.ts
@@ -0,0 +1,35 @@
+import {  ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { NotFoundComponent } from './not-found.component';
+import { NotFoundModule } from './not-found.module';
+
+describe('NotFoundComponent', () => {
+    let component: NotFoundComponent;
+    let fixture: ComponentFixture<NotFoundComponent>;
+
+    beforeEach(async() => {
+        await TestBed.configureTestingModule({
+            imports: [NotFoundModule, 
+                TranslateModule.forRoot({
+                    loader : {
+                        provide: TranslateLoader,
+                        useClass: TranslateFakeLoader
+                    }
+                })],
+            providers: [
+                TranslateService
+            ]
+        }).compileComponents();
+    }); 
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NotFoundComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
