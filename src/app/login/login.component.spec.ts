dde9f22292c4842ebf07f5019ce099679db2ed98
Jason Levine <levjason@gmail.com>
2020-10-29 14:41:03 -0400

    TranslateModule to login component test

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { LoginComponent } from './login.component';
 import { LoginModule } from './login.module';
+
 
 describe('LoginComponent', () => {
     let component: LoginComponent;
     let fixture: ComponentFixture<LoginComponent>;
       beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [LoginModule, RouterTestingModule, BrowserAnimationsModule]
+            imports: [LoginModule, RouterTestingModule, BrowserAnimationsModule, TranslateModule]
         }).compileComponents();
     }); 
     beforeEach(() => {
